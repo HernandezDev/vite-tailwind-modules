e2a71697a8a6a337b1b83493dcfe3e19bda5acbd
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2025-09-27 00:14:49 -0300

    Add tailwind-merge dependency and implement twclass for dynamic class management

--- src/modules/contador.js
+++ src/modules/contador.js
@@ -1,12 +1,15 @@
+import twclass from './twclass.js';
 export default class Contador {
   constructor(element) {
     this.element = element;
+    this.twclass = new twclass(this.element);
     element.addEventListener('click', () => {
       this.increment();
      });
     }
   increment() {
     this.element.textContent = parseInt(this.element.textContent) + 1;
+    this.twclass.addClass('bg-green-500 hover:bg-green-700');
   }
 }
 
