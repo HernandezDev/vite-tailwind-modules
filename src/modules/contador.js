85af94461cd4d31e2b52d7210c0366ba0bf04a20
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2025-09-27 00:53:44 -0300

    Fix class name casing for twClass in contador module

--- src/modules/contador.js
+++ src/modules/contador.js
@@ -2,14 +2,14 @@ import twClass from './twclass.js';
 export default class Contador {
   constructor(element) {
     this.element = element;
-    this.twclass = new twClass(this.element);
+    this.twClass = new twClass(this.element);
     element.addEventListener('click', () => {
       this.increment();
      });
     }
   increment() {
     this.element.textContent = parseInt(this.element.textContent) + 1;
-    this.twclass.addClass('bg-green-500 hover:bg-green-700');
+    this.twClass.addClass('bg-green-500 hover:bg-green-700');
   }
 }
 
